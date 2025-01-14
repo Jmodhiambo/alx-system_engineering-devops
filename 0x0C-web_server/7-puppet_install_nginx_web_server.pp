a1634d8d43268bd8b4fb60041ce1dad3629cdaaa
jmodhiambo <jmodhiambo9346@gmail.com>
2025-01-15 02:25:07 +0300

    Installing Nginx web server with Puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,25 @@
-# Using Puppet| Install Nginx server, setup and configuration
+# Install Nginx package
 
 package { 'nginx':
-  ensure => 'installed'
+  ensure => 'installed',
 }
 
+# Replace the default page with "Hello World!"
 file { '/var/www/html/index.html':
+  ensure  => 'file',
   content => 'Hello World',
 }
 
-file_line { 'redirection-301':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+# Configure Nginx server to redirect /redirect_me with a 301 Moved Permanently
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => template('nginx/default_redirect.pp.erb'),  # Use template to include the proper server configuration
+  notify  => Service['nginx'],  # Notify service to restart if the file changes
 }
 
+# Ensure Nginx service is running
 service { 'nginx':
-  ensure  => running,
+  ensure  => 'running',
+  enable  => true,
   require => Package['nginx'],
 }
