c5f189a29cfbf7e2ff47d7fdb1aa0566b65a6e0f
jmodhiambo <jmodhiambo9346@gmail.com>
2025-01-15 02:19:36 +0300

    Installing Nginx web server with Puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,62 +1,21 @@
-# Puppet manifest to install Nginx and configure as per requirements
-node default {
-    # Ensure the Nginx package is installed
-    package { 'nginx':
-        ensure => installed,
-    }
+# Using Puppet| Install Nginx server, setup and configuration
 
-    # Ensure Nginx service is running and enabled
-    service { 'nginx':
-        ensure => running,
-        enable => true,
-    }
+package { 'nginx':
+  ensure => 'installed'
+}
 
-    # Replace the default page with "Hello World!"
-    file { '/var/www/html/index.nginx-debian.html':
-        ensure  => file,
-        content => "Hello World!\n",
-        owner   => 'www-data',
-        group   => 'www-data',
-        mode    => '0644',
-    }
+file { '/var/www/html/index.html':
+  content => 'Hello World',
+}
 
-    # Configure the Nginx default site to include 301 redirect
-    file { '/etc/nginx/sites-available/default':
-        ensure  => file,
-        content => @("END"),
-        server {
-            listen 80 default_server;
-            listen [::]:80 default_server;
+file_line { 'redirection-301':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
 
-            root /var/www/html;
-            index index.html index.nginx-debian.html;
-
-            server_name _;
-
-            location / {
-                try_files \$uri \$uri/ =404;
-            }
-
-            # 301 redirect for /redirect_me
-            location = /redirect_me {
-                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-            }
-        }
-        END
-        notify  => Service['nginx'],
-    }
-
-    # Validate Nginx configuration
-    exec { 'nginx -t':
-        command     => '/usr/sbin/nginx -t',
-        refreshonly => true,
-        subscribe   => File['/etc/nginx/sites-available/default'],
-    }
-
-    # Restart Nginx to apply changes
-    exec { 'restart_nginx':
-        command     => '/usr/sbin/service nginx restart',
-        refreshonly => true,
-        subscribe   => Exec['nginx -t'],
-    }
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
