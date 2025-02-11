e180c5b26770149ccad1915ea877a781117b213e
jmodhiambo <jmodhiambo9346@gmail.com>
2025-02-11 05:08:44 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,23 +6,25 @@ his/her TODO list progress.
 from sys import argv
 import requests
 
-user_id = int(argv[1])
 
-emp_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+if __name__ == "__main__":
+    user_id = int(argv[1])
 
-# Gets the employee data and their todo list.
-employee = requests.get(emp_url).json()
-todos = requests.get(todo_url).json()
+    emp_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
 
-# Finds the completed and total task for the specific employee
-completed_task = len([task for task in todos if task["completed"]])
-total_task = len(todos)
+    # Gets the employee data and their todo list.
+    employee = requests.get(emp_url).json()
+    todos = requests.get(todo_url).json()
 
-print("Employee {} is done with tasks({}/{}):"
-      .format(employee["name"], completed_task, total_task))
+    # Finds the completed and total task for the specific employee
+    completed_task = len([task for task in todos if task["completed"]])
+    total_task = len(todos)
 
-# Prints the tasks completed by the employee
-for task in todos:
-    if task["completed"]:
-        print("\t {}".format(task["title"]))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee["name"], completed_task, total_task))
+
+    # Prints the tasks completed by the employee
+    for task in todos:
+        if task["completed"]:
+            print("\t {}".format(task["title"]))
