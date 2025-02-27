6c90de7f421e23692660326fa13220636937a48e
jmodhiambo <jmodhiambo9346@gmail.com>
2025-02-27 08:52:09 +0600

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,21 @@
 # Puppet manifest to fix Apache 500 errors by installing necessary PHP modules and correcting WordPress file permissions
 
-exec { 'fix-wordpress':
-    command     => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-    refreshonly => '/bin'
+# Ensure PHP and MySQL modules are installed
+package { ['php', 'php-mysql', 'libapache2-mod-php']:
+    ensure => installed,
+}
+
+# Fix permissions for the WordPress directory
+file { '/var/www/html':
+    ensure  => directory,
+    recurse => true,
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0755',
+}
+
+# Restart Apache to apply changes
+exec { 'restart_apache':
+    command     => '/etc/init.d/apache2 restart',
+    refreshonly => true,
 }
