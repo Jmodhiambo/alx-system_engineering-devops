c082cd89389a7a95dd5058ac77d4f86f5d397621
jmodhiambo <jmodhiambo9346@gmail.com>
2025-03-01 07:30:11 +0300

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -4,25 +4,3 @@ exec { 'fix-wordpress':
         command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
         path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
 }
-
-# Ensure PHP and necessary modules are installed
-package { 'php':
-        ensure => present,
-}
-
-package { 'libapache2-mod-php':
-        ensure  => present,
-        require => Package['php'],
-}
-
-package { 'php-mysql':
-        ensure  => present,
-        require => Package['php'],
-}
-
-# Run apt-get update to resolve package location issues
-exec { 'apt-update':
-        command => '/usr/bin/apt-get update',
-        path    => ['/bin', '/usr/bin/', '/usr/local/bin/'],
-        before  => Package['php'],
-}
