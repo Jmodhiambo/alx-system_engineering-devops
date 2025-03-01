0c6936b31a063daef4b44ef017cee2d62165836f
jmodhiambo <jmodhiambo9346@gmail.com>
2025-03-01 07:23:08 +0300

    Puppet 02

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,28 @@
 # Puppet manifest to fix Apache 500 errors by installing necessary PHP modules and correcting WordPress file permissions
 
-# Remove duplicate sources and update apt
 exec { 'fix-wordpress':
-    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-    path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+        command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+        path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+}
+
+# Ensure PHP and necessary modules are installed
+package { 'php':
+        ensure => present,
+}
+
+package { 'libapache2-mod-php':
+        ensure  => present,
+        require => Package['php'],
+}
+
+package { 'php-mysql':
+        ensure  => present,
+        require => Package['php'],
+}
+
+# Run apt-get update to resolve package location issues
+exec { 'apt-update':
+        command => '/usr/bin/apt-get update',
+        path    => ['/bin', '/usr/bin/', '/usr/local/bin/'],
+        before  => Package['php'],
 }
