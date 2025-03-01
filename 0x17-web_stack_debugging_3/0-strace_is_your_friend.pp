1210c84646ac2932fbdf3dc7beae323974fb3a5d
jmodhiambo <jmodhiambo9346@gmail.com>
2025-03-01 07:01:18 +0300

    Puppet 01

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,28 +1,7 @@
 # Puppet manifest to fix Apache 500 errors by installing necessary PHP modules and correcting WordPress file permissions
 
 # Remove duplicate sources and update apt
-exec { 'remove_duplicate_sources':
-    command => '/bin/sed -i.bak "/^deb .* trusty-backports/ d" /etc/apt/sources.list && apt-get update',
-    path    => ['/bin', '/usr/bin'],
-    onlyif  => '/bin/grep -q "trusty-backports" /etc/apt/sources.list',
-}
-
-# Ensure PHP and MySQL modules are installed
-package { ['php', 'php-mysql', 'libapache2-mod-php']:
-    ensure => installed,
-}
-
-# Fix permissions for the WordPress directory
-file { '/var/www/html':
-    ensure  => directory,
-    recurse => true,
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0755',
-}
-
-# Restart Apache to apply changes
-exec { 'restart_apache':
-    command     => '/etc/init.d/apache2 restart',
-    refreshonly => true,
+exec { 'fix-wordpress':
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/usr/local/bin/:/bin/'
 }
