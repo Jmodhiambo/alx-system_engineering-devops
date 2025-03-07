3c753ce6c0c61c8a8266ad2c090fe8a336be4210
jmodhiambo <jmodhiambo9346@gmail.com>
2025-03-07 04:16:02 +0300

    Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -10,9 +10,12 @@ service { 'nginx':
   require => Package['nginx'],
 }
 
-# Fix configuration settings for optimal performance
+# Fix configuration settings for optimal performance, with proper ownership and permissions
 file { '/etc/nginx/nginx.conf':
   ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
   content => template('path_to_your_nginx_template.erb'),
   require => Package['nginx'],
   notify  => Service['nginx'],
@@ -23,4 +26,11 @@ exec { 'reload-nginx':
   command     => '/usr/sbin/nginx -s reload',
   refreshonly => true,
   subscribe   => File['/etc/nginx/nginx.conf'],
+}
+
+# Example for log monitoring (e.g., configuring log rotation for Nginx logs)
+file { '/etc/logrotate.d/nginx':
+  ensure  => file,
+  content => template('path_to_your_logrotate_template.erb'),
+  require => Package['nginx'],
 }
