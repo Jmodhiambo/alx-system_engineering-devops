57668ad23add4cc8a7e67421422c89b4351ab463
jmodhiambo <jmodhiambo9346@gmail.com>
2025-03-07 04:23:05 +0300

    Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,36 +1,5 @@
-# Ensure Nginx is installed
-package { 'nginx':
-  ensure => installed,
+# Fix Nginx limits
+exec { 'Limit':
+  command => '/usr/bin/env sed -i s/15/2000/ /etc/default/nginx',
 }
-
-# Ensure Nginx service is running
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
-}
-
-# Fix configuration settings for optimal performance, with proper ownership and permissions
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => template('path_to_your_nginx_template.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Reload Nginx to apply changes
-exec { 'reload-nginx':
-  command     => '/usr/sbin/nginx -s reload',
-  refreshonly => true,
-  subscribe   => File['/etc/nginx/nginx.conf'],
-}
-
-# Example for log monitoring (e.g., configuring log rotation for Nginx logs)
-file { '/etc/logrotate.d/nginx':
-  ensure  => file,
-  content => template('path_to_your_logrotate_template.erb'),
-  require => Package['nginx'],
-}
+exec { '/usr/bin/env service nginx restart': }
