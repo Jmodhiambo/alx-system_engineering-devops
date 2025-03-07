771a35f48c5fad83e6fb440f5514836b7007be9b
jmodhiambo <jmodhiambo9346@gmail.com>
2025-03-07 04:10:57 +0300

    Puppet 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,41 +1,26 @@
-ure Nginx service is running
-    service { 'nginx':
-        ensure     => running,
-        enable     => true,
-        hasrestart => true,
-    }
+# Ensure Nginx is installed
+package { 'nginx':
+  ensure => installed,
+}
 
-    # Configure Nginx to handle high traffic
-    file { '/etc/nginx/nginx.conf':
-        ensure  => file,
-        content => template('nginx/nginx.conf.erb'),
-        notify  => Service['nginx'],
-    }
+# Ensure Nginx service is running
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
 
-    # Set optimized Nginx configuration
-    file { '/etc/nginx/conf.d/optimization.conf':
-        ensure  => file,
-        content => """
-        worker_processes auto;
-        events {
-            worker_connections 4096;
-            multi_accept on;
-        }
-        http {
-            keepalive_timeout 65;
-            client_max_body_size 10m;
-            client_body_buffer_size 16k;
-            proxy_buffers 16 16k;
-            proxy_buffer_size 16k;
-            proxy_busy_buffers_size 64k;
-        }
-        """,
-        notify  => Service['nginx'],
-    }
+# Fix configuration settings for optimal performance
+file { '/etc/nginx/nginx.conf':
+  ensure  => file,
+  content => template('path_to_your_nginx_template.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
 
-    # Reload Nginx to apply changes
-    exec { 'reload-nginx':
-        command     => '/etc/init.d/nginx reload',
-        refreshonly => true,
-        subscribe   => File['/etc/nginx/conf.d/optimization.conf'],
-    }
+# Reload Nginx to apply changes
+exec { 'reload-nginx':
+  command     => '/usr/sbin/nginx -s reload',
+  refreshonly => true,
+  subscribe   => File['/etc/nginx/nginx.conf'],
+}
