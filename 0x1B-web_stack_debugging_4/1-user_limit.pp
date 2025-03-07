9890c64c7fdb61c1e771072659b1cc303e6770c2
jmodhiambo <jmodhiambo9346@gmail.com>
2025-03-07 04:43:01 +0300

    Puppet 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,14 +1,22 @@
-# Increase file descriptor limits for holberton user
-
-file { '/etc/security/limits.d/holberton.conf':
-    ensure  => file,
-    content => @("EOT")
-      holberton hard nofile 65535
-      holberton soft nofile 65535
-    |-EOT-|
+# Increase file descriptor limits for the OS to prevent "Too many open files" errors
+exec { 'update-limits-conf':
+  command => '/usr/bin/env echo "* - nofile 65535" >> /etc/security/limits.conf',
+  unless  => '/bin/grep -q "* - nofile 65535" /etc/security/limits.conf',
 }
 
+# Update PAM (Pluggable Authentication Module) configuration for limits
 exec { 'update-pam-limits':
-    command => '/usr/bin/env bash -c "echo session required pam_limits.so >> /etc/pam.d/common-session"',
-    unless  => '/bin/grep -q "pam_limits.so" /etc/pam.d/common-session',
+  command => '/usr/bin/env echo "session required pam_limits.so" >> /etc/pam.d/common-session',
+  unless  => '/bin/grep -q "session required pam_limits.so" /etc/pam.d/common-session',
+}
+
+exec { 'update-pam-limits-noninteractive':
+  command => '/usr/bin/env echo "session required pam_limits.so" >> /etc/pam.d/common-session-noninteractive',
+  unless  => '/bin/grep -q "session required pam_limits.so" /etc/pam.d/common-session-noninteractive',
+}
+
+# Restart the system services to apply changes
+exec { 'restart-services':
+  command => '/usr/bin/env systemctl restart sshd',
+  require => [Exec['update-limits-conf'], Exec['update-pam-limits'], Exec['update-pam-limits-noninteractive']],
 }
