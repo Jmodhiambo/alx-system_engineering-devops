a094249c131744718e2dc3dff336a7b76f4974e0
jmodhiambo <jmodhiambo9346@gmail.com>
2025-03-07 04:59:35 +0300

    Puppet 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,10 @@
-# Update open file limits for all users
-exec { 'Fix open file limits':
-  command => '/usr/bin/env sed -i "s/1024/20000/; s/4096/20000/" /etc/security/limits.conf',
+# Fix open files limit
+exec { 'Fix hard and soft limits':
+    command => '/usr/bin/env sed -i "s/4/20000/; s/5/20000/" /etc/security/limits.conf',
+}
+
+# Enable PAM limits without visible output
+exec { 'Enable PAM limits':
+    command => '/bin/sh -c "echo \'session required pam_limits.so\' >> /etc/pam.d/common-session"',
+    unless  => '/bin/grep -q "pam_limits.so" /etc/pam.d/common-session"',
 }
